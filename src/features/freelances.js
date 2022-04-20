f4745d66fa1f754e99f8ed86422e3f0d211c67f5
DevWeb13 <giuliano.loic@sfr.fr>
2022-04-20 09:48:10 +0200

    feat: freelances.js
    Create function freelancesReducer(state = initialState, action)

--- src/features/freelances.js
+++ src/features/freelances.js
@@ -1,3 +1,5 @@
+import produce from 'immer';
+
 const initialState = {
   status: 'void',
   data: null,
@@ -14,3 +16,43 @@ const freelancesResolved = (data) => ({ type: RESOLVED, payload: data });
 const freelancesRejected = (error) => ({ type: REJECTED, payload: error });
 
 export default function freelancesReducer(state = initialState, action) {
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case FETCHING: {
+        if (draft.status === 'void') {
+          draft.status = 'pending';
+          return;
+        }
+        if (draft.status === 'rejected') {
+          draft.status = 'pending';
+          draft.error = null;
+          return;
+        }
+        if (draft.status === 'resolved') {
+          draft.status = 'updating';
+          return;
+        }
+        return;
+      }
+      case RESOLVED: {
+        if (draft.status === 'updating' || draft.status === 'pending') {
+          draft.status = 'resolved';
+          draft.data = action.payload;
+          return;
+        }
+        return;
+      }
+      case REJECTED: {
+        if (draft.status === 'updating' || draft.status === 'pending') {
+          draft.status = 'rejected';
+          draft.error = action.payload;
+          draft.data = null;
+          return;
+        }
+        return;
+      }
+      default:
+        return;
+    }
+  });
+}
